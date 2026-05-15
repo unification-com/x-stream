f91131406d067f401caa580c418876911a2c6a80
Codegnosis <paul@codegnosis.co.uk>
2026-05-15 13:08:35 +0100

    x/stream - import full module source tree from mainchain

---
+++ x/stream/keeper/params_test.go
@@ -0,0 +1,60 @@
+package keeper_test
+
+import (
+	mathmod "cosmossdk.io/math"
+
+	"github.com/unification-com/x-stream/x/stream/types"
+)
+
+func (s *KeeperTestSuite) TestParams() {
+	testCases := []struct {
+		name      string
+		input     types.Params
+		expectErr bool
+	}{
+		{
+			name: "set full valid params",
+			input: types.Params{
+				ValidatorFee: mathmod.LegacyNewDecWithPrec(24, 2),
+			},
+			expectErr: false,
+		},
+		{
+			name: "set invalid params > 100%",
+			input: types.Params{
+				ValidatorFee: mathmod.LegacyNewDecWithPrec(101, 2),
+			},
+			expectErr: true,
+		},
+		{
+			name: "set invalid params negative value",
+			input: types.Params{
+				ValidatorFee: mathmod.LegacyNewDecWithPrec(-1, 2),
+			},
+			expectErr: true,
+		},
+		{
+			name: "set invalid params nil value",
+			input: types.Params{
+				ValidatorFee: mathmod.LegacyDec{},
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			expected := s.app.StreamKeeper.GetParams(s.ctx)
+			err := s.app.StreamKeeper.SetParams(s.ctx, tc.input)
+			if tc.expectErr {
+				s.Require().Error(err)
+			} else {
+				expected = tc.input
+				s.Require().NoError(err)
+			}
+
+			p := s.app.StreamKeeper.GetParams(s.ctx)
+			s.Require().Equal(expected, p)
+		})
+	}
+}
